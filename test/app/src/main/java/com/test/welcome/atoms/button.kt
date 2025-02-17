7245901a58e5da8fbb2463e5ecdfe62d9d00cf78
Mihai Gutu <123064601+mikhailgutsu@users.noreply.github.com>
2025-02-17 15:05:42 +0200

    TEST: RE-Design the Button from new /drawable/custome_button.xml

--- test/app/src/main/java/com/test/welcome/atoms/button.kt
+++ test/app/src/main/java/com/test/welcome/atoms/button.kt
@@ -1,15 +1,19 @@
 package com.test.welcome.atoms
 
+import com.test.R
+
 import android.content.Context
 import android.widget.Toast
 import android.view.Gravity
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 
 class button(context: Context) : androidx.appcompat.widget.AppCompatButton(context) {
     init {
         text = "START"
         setPadding(50, 50, 50, 50)
         gravity = Gravity.CENTER
+        background = ContextCompat.getDrawable(context, R.drawable.custom_button)
 
         layoutParams = LinearLayout.LayoutParams(
             (resources.displayMetrics.widthPixels * 0.8).toInt(),
